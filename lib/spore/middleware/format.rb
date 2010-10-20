49022c93f49ba5741eca4a047db4319b719e38f4
Alexis Sukrieh <sukria@sukria.net>
2010-10-20 16:20:31 +0200

    Spore::Middleware::Format

---
+++ lib/spore/middleware/format.rb
@@ -0,0 +1,40 @@
+require 'json'
+require 'yaml'
+require 'spore/middleware'
+
+# I don't like to do that, but we have to monkeypatch the class of Net::HTTPOK
+# because we can't overwrite its body attribute otherwise
+module Net
+  class HTTPOK
+    def body=(value)
+      @body = value
+    end
+  end
+end
+
+class Spore
+  class Middleware
+    class Format < Spore::Middleware
+      
+      class UnsupportedFormat < Exception
+      end
+
+      def expected_params
+        [ :format ]
+      end
+
+      def process_response(resp)
+        if self.format.downcase == 'json'
+          resp.body = JSON.parse(resp.body)
+        elsif self.format.match(/\.ya?ml$/i)
+          resp.body = YAML.load(resp.body)
+        else
+          raise UnsupportedFormat, "don't know how to handle this format '#{self.format}'"
+        end
+
+        return resp
+      end
+
+    end
+  end
+end
