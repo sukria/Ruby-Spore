a527de44dcb55352f40fe2b656571632aca6164b
Alexis Sukrieh <sukria@sukria.net>
2010-10-20 16:40:10 +0200

    FIX Spore::Middleware::Runtime

--- lib/spore/middleware/runtime.rb
+++ lib/spore/middleware/runtime.rb
@@ -1,15 +1,16 @@
-module Spore
-  module Middleware
+class Spore
+  class Middleware
 
     class Runtime < Spore::Middleware
       def process_request(env)
-        env['spore.middleware.runtime.start'] = Time.now
+        env['sporex.runtime.start'] = Time.now
         return nil
       end
 
-      def process_response(resp)
-        elapsed_time = Time.now - env['spore.middleware.runtime.start']
+      def process_response(resp, env)
+        elapsed_time = Time.now - env['sporex.runtime.start']
         resp.add_field('X-Spore-Runtime', elapsed_time.to_s)
+        return resp
       end
     end
 
