b5068ef7bb0a5d1102e713df502897c2b44f3ee6
Alexis Sukrieh <sukria@sukria.net>
2010-10-20 16:20:41 +0200

    test for the Format middleware

--- test/test_github.rb
+++ test/test_github.rb
@@ -1,17 +1,33 @@
 require "rubygems"
 require "test/unit"
 require "spore"
+require 'net/http'
 
+require 'spore/middleware/format'
 
 class TestGitHub < Test::Unit::TestCase
 
-  def test_github_search
+  def test_basic_github_search
     github_spec = File.join(File.dirname(__FILE__), 'github.json')
     gh = Spore.new(github_spec)
 
     r = gh.user_search(:format => 'json', :search => 'sukria')
+    assert_kind_of Net::HTTPOK, r
+    assert_kind_of String, r.body
+  end
 
-    puts "resp: #{r.to_yaml}"
+  def test_with_format_github_search
+    github_spec = File.join(File.dirname(__FILE__), 'github.json')
+    gh = Spore.new(github_spec)
+    
+    gh.enable(Spore::Middleware::Format, :format => 'json')
+
+    assert_equal 1, gh.middlewares.size
+
+    r = gh.user_search(:format => 'json', :search => 'sukria')
+    assert_kind_of Net::HTTPOK, r
+    assert_kind_of Hash, r.body
+    assert_equal 'sukria', r.body['users'][0]['name']
   end
 
 end
