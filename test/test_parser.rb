b6389e99699de77b3fc3d732d8357bc52b23faf7
hallelujah <hery@rails-royce.org>
2010-10-21 16:57:56 +0200

    Allow loading a custom parser and do not load json if not needed
    Implements a Spore.load_parser class method to load another parser
      Example given in test/test_parser.rb
    
      Spore.load_parser takes two arguments specs and options
        if spec is a yml or json file options is skipped
        else options is used for requiring and loading the correct parser
    
         options is a Hash with :require and :parser keys.
          :require is a file to require
          :parser is a String to pass in Object.const_get

---
+++ test/test_parser.rb
@@ -0,0 +1,29 @@
+require 'helper'
+require 'spore/spec_parser/yaml'
+require 'spore/spec_parser/json'
+require 'rexml/document'
+class TestParser < Test::Unit::TestCase
+
+  def test_yaml_parser
+    spec = File.expand_path('../github.yml', __FILE__)
+    specs = Spore::SpecParser::Yaml.load_file(spec)
+    assert_instance_of Hash, specs
+    assert specs.has_key?('name')
+  end
+
+  def test_json_parser
+    spec = File.expand_path('../github.json', __FILE__)
+    specs = Spore::SpecParser::Json.load_file(spec)
+    assert_instance_of Hash, specs
+    assert specs.has_key?('name')
+  end
+
+  def test_custom_parser
+    file = File.expand_path('../xml_parser.rb', __FILE__)
+    spec = File.expand_path('../github.xml',__FILE__)
+    parser = Spore.load_parser(spec, :require => file, :parser => 'MyCustomParser')
+    specs = parser.load_file(spec)
+    assert_instance_of Hash, specs
+    assert specs.has_key?('name')
+  end
+end
---
+++ test/xml_parser.rb
@@ -0,0 +1,6 @@
+require 'xmlsimple'
+class MyCustomParser
+  def self.load_file(spec)
+    XmlSimple.xml_in(File.read(spec))
+  end
+end
